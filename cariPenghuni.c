a6461ac2af3625b4baf26fd5071da0f2c647a413
Gioxaa <gioxatesting6@gmail.com>
2025-11-24 14:25:26 +0700

    feat: Implement resident search functionality and room clearing logic.

--- cariPenghuni.c
+++ cariPenghuni.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include "header/kamar.h"
 
+
+int stringCompare(char str1[], char str2[]) {
+    int i = 0;
+    while (str1[i] == str2[i]) {
+        if (str1[i] == '\0' || str2[i] == '\0') {
+            break;
+        }
+        i++;
+    }
+    return str1[i] - str2[i];
+}
+
 void cari_penghuni(kamar ruangan[], int jumlah) {
+    int i;
+    char nama[100];
     
+    printf("Masukkan Nama yang ingin dicari: ");
+    scanf("%s", &nama);
+
+    if(stringCompare(nama, ruangan[i].nama) == 0) {
+        printf("Nama Penghuni: %s\n", ruangan[i].nama);
+        printf("Nomor Kamar: %d\n", ruangan[i].nomor);
+        printf("Status Bayar: %s\n", ruangan[i].statusBayar == 1 ? "Lunas" : "Belum Lunas");
+        printf("Status Kamar: %s\n", ruangan[i].statusKamar == 1 ? "Terisi" : "Kosong");
+    } else {
+        printf("Pe--nghuni tidak ditemukan\n");
+    }
 }
--- kosongkanKamar.c
+++ kosongkanKamar.c
@@ -2,5 +2,30 @@
 #include "header/kamar.h"
 
 void kosongkan_kamar(kamar ruangan[], int jumlah) {
+    int i=0;
+    int noKamar;
+    int index;
+
+    noKamar = index + 1;
+
+    printf("Masukkan nomor kamar yang ingin di kosongkan: ");
+    scanf("%d", &noKamar);
+
+    if(noKamar < 1 || noKamar > jumlah) {
+        printf("\nNomor kamar tidak valid\n");
+        return;
+    }
+
+    index = noKamar - 1;
+    if(ruangan[index].statusKamar == 0) {
+        printf("\nKamar %d kosong\n", noKamar);
+        return;
+    }
+
+    ruangan[index].nama[0] = '\0';
+    ruangan[index].statusBayar = 0;
+    ruangan[index].statusKamar = 0;
+    printf("\nKamar %d berhasil di kosongkan\n", noKamar);
+
     
 }
